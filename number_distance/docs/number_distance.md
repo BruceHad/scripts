44fab2ca33c136f62d62f879be487546b90229e7
Bruce Hadden <bahadden@gmail.com>
2016-05-12 14:30:55 +0000

    Improvements to number_distance: Cache the numbers file. Segment the processing and write file periodically.

--- number_distance/docs/number_distance.md
+++ number_distance/docs/number_distance.md
@@ -1,34 +1,34 @@
 # Numbers Available
 
-_Number_ is an of the set of five digit numbers, excluding those that start with zero. e.g. 10235 but not 01265. So a total set of 90,000 numbers.
+## Problem
 
-New numbers are assigned and resigned over time so that at the present moment there are nearly 8,000 active and 14,000 resigned numbers, so around 22,000 numbers have been 'used' already.
+The _number_ is the set of five digit numbers excluding those that start with zero. e.g. 10235 but not 01265. So the total set contains 90,000 numbers ranging from 10000 to 99999.
 
-Number can't be used more than once, so there are around 68,000 numbers available (90,000 - 22,000).
+New numbers are assigned and resigned over time. So far, around 22,000 numbers have been assigned, but 14,000 have since resigned, leaving 8,000 numbers active and in use, and around 68,000 numbers still available for use.
 
-Numbers are entered by hand or by OCR reading of a form.
+A _misread_ is where a number is entered incorrectly for some reason (usually OCR misread or user error). A _collision_ is a misread that happens to match one of the active numbers.
 
-A _collision_ is where a number is entered incorrectly for some reason (usually OCR misread or user error) _and_ where the incorrectly entered number matches a valid, active number.
+The problem is how to find and assign new numbers that minimise the risk of collisions.
 
-For example if 12345 is misentered as 72345 that is a collision if 72345 is an active list number. If not, it is a simple misread.
-
-The aim is to select new numbers that minimise the risk of clashes.
-
-## Method
-
-Since we are comparing the numbers as we would strings, all numbers are converting to string types.
+## String Distance
 
 The [Hamming Distance](https://en.wikipedia.org/wiki/Hamming_distance) compares two strings of the same length, and counts the number of positions at which the corresponding symbols are different. I.e. it measures the minimum number of errors that could have transformed one string into the other.
 
-So the pair 11111 & 21111 has a Hamming Distance of 1, since only one digit requires changing to create a match.
+So 11111 & 21111 have a Hamming Distance of 1, while 11111 & 22221 have a hamming distance of 4.
 
-We can calculate the hamming distance for each available list number against all active list numbers and count up the scores.
+## Method
 
-So for example:
+We can calculate the Hamming Distance for each available number against each active list numbers. Any number that has no other active number with a Hamming Distance of 1 (i.e. 1 transformation away) would be considered a 'hit'. This would ensure that two or more characters would have to be misread before any possibility of a collision arises, hopefully reducing the risk significantly.
 
-number	zeros	ones	twos	threes	fours	fives
-10001	0		3		49		490		2312	4345
+When comparing to 'active' list numbers, we also have to take into account 'hits' that have been found already (i.e. numbers that will be 'active' in the future), to ensure that we don't add two new numbers that are only 1 transformation away from each other (no idea how likely this is). Therefore each 'hit' should be added to the list of active numbers, before any further checks are made.
 
-This number has zero 'zeros', i.e. no exact matches (which is what we would expect).
+We also have to be careful in defining 'active' numbers, as they can remain in use up to 15 months after being resigned.
 
-But there are 3 active list number within 1 transformation from it. e.g. 10000, 10901 and 10501.
+For a five digit number there are approximately 50 other numbers within 1 transform from the original. 
+
+## Performance
+
+There are around 68,000 potential numbers, which have to be compared to the 8,000 active numbers. A full analysis of each number would result in 544,000,000 comparisons.
+
+Testing shows that the hamming distance comparison take around 5x10**-6 seconds which is pretty quick, but 544 million comparisons still take over an hour to process.
+
--- number_distance/numbers.py
+++ number_distance/numbers.py
@@ -1,3 +1,5 @@
+import random, os.path
+
 def read_file(filename):
 	"""Reads a textfile and returns it as a list (of strings)
 		
@@ -8,80 +10,174 @@ def read_file(filename):
 	results = []
 	for line in f:
 		number = line.rstrip('\n')
-		results.append(number)
-		
+		results.append(number)	
 	f.close()
 	return results
 
-def get_numbers(exclude):
+def write_file(filename, data):
+	f = open(filename, 'w')
+	for line in data:
+		f.write(line+'\n')
+	f.close()
+
+def write_data(data, filename, append = False, header = False):
+	"""Writes a list of dicts to a csv file.
+	Option to:
+		append to existing file or create a new file
+		include a header based on dict keys
+
+	e.g write_data([{'one': 1, 'two': 2}, {'one': 1, 'two': 2}])
+	"""
+	first = True	
+	if append:	f = open(filename+'.csv', 'a')
+	else: f = open(filename+'.csv', 'w')
+	for line in data:
+		if first and header:
+			count = 0
+			for key,value in line.items():
+				f.write(str(key) + ',')
+				count += 1
+				if(count == len(line)):
+					first = False
+					break;
+			f.write('\n')
+		for key, value in line.items():
+			# print rest of data
+			f.write('\"' + str(value) + '\",')
+		f.write('\n')
+	f.close()
+
+def get_numbers(exclude, cache = True):
 	"""Provides a list of unique numbers (as strings), 5 digits long
 		exluding any that start with zero and any provide 
 		by in the 'exclude' list
 
-	>>> len(get_numbers(['10000', '99999']))
+		If cache true, check for existing file first. If not available, generate list and write to cache file for next
+		run.
+		
+	>>> len(get_numbers(['10000', '99999'], False))
 	89998
 	"""	
+	filename = 'get_numbers_cache.txt'
 	numbers = []
-	for i in range(10000, 100000):
-		if str(i) not in exclude:
-			numbers.append(str(i))
+	if cache and os.path.isfile(filename):
+		numbers = read_file(filename)
+	else:
+		for i in range(10000, 100000):
+			if str(i) not in exclude:
+				numbers.append(str(i))	
+	if cache:
+		write_file(filename, numbers)
+		
 	return numbers
 	
 
 	
 def hammingDistance(s1, s2):
-    """Return the Hamming distance between equal-length sequences
+	"""Return the Hamming distance between equal-length sequences
 	"""
-    if len(s1) != len(s2):
-        raise ValueError("Undefined for sequences of unequal length")
-    return sum(bool(ord(ch1) - ord(ch2)) for ch1, ch2 in zip(s1, s2))
+	if len(s1) != len(s2):
+		raise ValueError("Undefined for sequences of unequal length")
+	return sum(bool(ord(ch1) - ord(ch2)) for ch1, ch2 in zip(s1, s2))
 
 def analyse_number(number, active_numbers):
 	"""Check the number against all the active numbers and 
 		return the count of distances
 	"""
-
 	analysis = {
-		'results': [0, 0, 0, 0, 0, 0],
-		'ones': []
+		'number': number,
+		'zero_transform': 0,
+		'one_transform': 0,
+		'two_transform': 0,
+		'three_transform': 0,
+		'four_transform': 0,
+		'five_transform': 0,
+		'one_transform_numbers': []
 	}
 	number = str(number)
 	for acn in active_numbers:
 		dist = hammingDistance(number, acn)
-		analysis['results'][dist] += 1
-		if dist == 1: analysis['ones'].append(acn)
-		#elif dist == 2: analysis['twos'].append(acn)
+		if dist == 0: 
+			analysis['zero_transform'] += 1
+		elif dist == 1: 
+			analysis['one_transform'] += 1
+			analysis['one_transform_numbers'].append(acn)
+		elif dist == 2: 
+			analysis['two_transform'] += 1
+		elif dist == 3: 
+			analysis['three_transform'] += 1
+		elif dist == 4: 
+			analysis['four_transform'] += 1
+		elif dist == 5: 
+			analysis['five_transform'] += 1
+		else:
+			console.log('Error: distance > 5')
 	return analysis
 
-def compare_numbers(active_numbers, available_numbers):
+def compare_numbers(compare_nos, candidate_nos, reinclude_candidates = True):
 	"""Run a comparison between all available numbers and 
 		active numbers and generate a results file
 	"""
-	f = open('results.csv', 'w')
-	f.write('list_number,zeros,ones,twos,threes,fours,fives, \n')
-	for avn in available_numbers:
-		results = analyse_number(avn, active_numbers)
-		zero, one, two, three, four, five = results['results']
-		f.write('%s, %s, %s, %s, %s, %s, %s, \n' % (avn, str(zero), str(one), str(two), str(three), str(four), str(five)))
-		if int(avn) > 15000: 
-			break
-	f.close()
+	print('Approx. Comparisons: %d' % (len(candidate_nos) * len(compare_nos)))
+	print('Available Numbers: %d' % len(candidate_nos))
+	
+	segments = 2000
+	filename = 'results3_'+str(len(candidate_nos))
+	results, count, runs = [], 0, 1
+	for candidate in candidate_nos:
+		count += 1
+		result = analyse_number(candidate, compare_nos)
+		if(result):
+			results.append(result)
+			if reinclude_candidates and result['one_transform'] == 0: 
+				compare_nos.append(candidate)
+		if count == segments:
+			print(runs, runs * segments)
+			if runs == 1: write_data(results, filename, False, True)
+			else: write_data(results, filename, True, False)
+			results, count = [], 0
+			runs += 1
+	# Print final set
+	print(runs, (runs-1) * segments + count)
+	write_data(results, filename, True, False)
+	
 
 
+		
+def test_performance(comparison_numbers):
+	n = 1000
+	#test hammingDistance
+	a = time.clock()
+	x, y = str(random.randrange(10000,99999)), str(random.randrange(10000,99999))
+	
+	for i in range(n):		
+		hammingDistance(x, y)
+	b = time.clock() - a
+	print(n, b, b/n)
 
+	# test analyse_number
+	a = time.clock()
+	for i in range(n):
+		analyse_number(x, comparison_numbers)
+	b = time.clock() - a
+	print(n*len(comparison_numbers), b, b/(n*len(comparison_numbers)))
+	
+	
 	
 if __name__ == '__main__':
 	import doctest, time
 	print(doctest.testmod())
-	print(time.clock())
+
+	active_numbers = read_file('active_ln.txt') 
+	all_numbers = read_file('all_ln.txt') 
+	candidates = get_numbers(all_numbers, True)
 	
-	active_numbers = read_file('active_ln.txt')
-	all_numbers = read_file('all_ln.txt')
-	#available_numbers = get_numbers(all_numbers)
-	print(time.clock())
+	# Test performance of hammingDistance
+	#test_performance(active_numbers)	
 	
-	print(analyse_number(10001, active_numbers))
-	print(time.clock())
+	# Lets check active numbers against themselves
+	# test_numbers = read_file('test_numbers.txt')
+	#comparison = compare_numbers(active_numbers, active_numbers, False)
+	compare_numbers(active_numbers, candidates, True)
 	
-	#compare_numbers(active_numbers, available_numbers)
-	#print(time.clock())
+	
